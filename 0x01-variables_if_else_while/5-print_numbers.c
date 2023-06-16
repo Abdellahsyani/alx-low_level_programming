f47bc5be17995b69dcd66dd521f5b69d5409942e
Abdellahsyani <syaniabdos@gmail.com>
2023-06-16 10:20:22 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all number digit
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+int digit = 0;
+while (digit <= 9)
+{
+printf("%i", digit);
+digit++;
+}
+printf('\n');
+return (0);
+}
