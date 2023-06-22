597860f4e31326c8f1e8956f271a2153b8653d5b
Abdellahsyani <syaniabdos@gmail.com>
2023-06-22 04:00:12 -0700

    main.putchar

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef HEADREFILE_H_INCLUDE
+#define HEADREFILE_H_INCLUDE
+
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
