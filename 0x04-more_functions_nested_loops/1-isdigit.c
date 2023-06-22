b7035773cf0b8ee3e371f7583fc36dd326f18282
Abdellahsayni <syaniabdos@gmail.com>
2023-06-22 05:19:53 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+/**
+ * _isdigit - print  number
+ * @c: number digit
+ * Return: 1 if c is digit, or 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if (c <= 9 && c > 9)
+		return (1);
+	else
+		return (0);
+}
