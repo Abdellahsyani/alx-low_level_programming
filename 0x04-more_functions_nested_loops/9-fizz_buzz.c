465a133f68cb8fda7a19fd2d59ec5145e7871248
Abdellahsayni <syaniabdos@gmail.com>
2023-06-23 08:35:29 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,18 +14,18 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz"); }
-			else if (i % 3 != 0 && i % 5 == 0)
-			{
-			printf(" Buzz"); }
-			else if (i % 3 == 0 && i % 5 == 0)
-			{
-			printf(" FizzBuzz"); }
-			else if (i == 1)
-			{
-			printf("%d", i); }
-			else
-			{
-			printf(" %d", i); }
+else if (i % 3 != 0 && i % 5 == 0)
+{
+printf(" Buzz"); }
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf(" FizzBuzz"); }
+else if (i == 1)
+{
+printf("%d", i); }
+else
+{
+printf(" %d", i); }
 		}
 printf("\n");
 	return (0);
