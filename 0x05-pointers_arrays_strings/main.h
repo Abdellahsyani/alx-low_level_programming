00b951a7548959f33879c6d824f41405a653e0ba
Abdellahsayni <syaniabdos@gmail.com>
2023-06-26 07:39:17 -0700

    main

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef HEADREFILE_H_INCLUDE
 #define HEADREFILE_H_INCLUDE
 
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
