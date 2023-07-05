9a2daf78cf549de7ff874f4f0bb592f66fbe7323
Abdellahsayni <syaniabdos@gmail.com>
2023-07-05 07:39:02 -0700

    mai

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
