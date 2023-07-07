d1e81ed3a69dffe6b2b8c10370692315008d91fa
Abdellahsayni <syaniabdos@gmail.com>
2023-07-07 10:57:15 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name, follow by new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+printf("%s \n", argv[0]);
+return (0);
+}
