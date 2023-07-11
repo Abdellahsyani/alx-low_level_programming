2ffa899617060e4dbce3a3e2f06d50aa3c5c525c
Abdellahsayni <syaniabdos@gmail.com>
2023-07-11 00:38:09 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,19 +5,20 @@
  * *create_array - print char and initializes it with a specific char
  * @c: input char
  * @size: input unisgned int
- * Return: 0 if size = 0 and NULL if set = NULL
+ * Return: 0 if size = 0 and NULL if str = NULL
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char *set;
+	char *str;
 
 	unisgned int i;
 
-	char *set = malloc(sizeof(char) * size);
+	char *str = malloc(sizeof(char) * size);
 
-	if (size == 0 || set == NULL)
+	if (size == 0 || str == NULL)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
-		set[i] = c;
-	return (set);
+		str[i] = c;
+	return (str);
