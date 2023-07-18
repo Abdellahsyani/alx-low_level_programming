e1f89e4a0e5d2cd2d35f50e261313ddba72e643f
Abdellahsayni <syaniabdos@gmail.com>
2023-07-18 00:14:31 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,53 +1,62 @@
 #include <stdlib.h>
-include "dog.h"
+#include "dog.h"
 
 /**
- * strlen - return the length of a string
- * @s: string to evaluate
+ * _strlen - returns the length of a string.
+ * @s: string to evaluate.
  *
- * Return: the length of the string
+ * Return: the length of the string.
  */
-int_strlen(char*s)
+int _strlen(char *s)
 {
 	int i;
-	
+
 	i = 0;
 
-	while (s[i]!= '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
+
 	return (i);
 }
+
 /**
- * *_strcpy - copies the string pointed to by src
- * including the terminating null byte (\0)
- * to the buffer pointed to by dest
- * @dest: pointr to the buffer in which we copy the string
- * @src: string to be copied
+ * *_strcpy - copies the string pointed to by src.
+ * including the terminating null byte (\0).
+ * To the buffer pointed to by dest.
+ * @dest: pointer to the buffer in which we copy the string.
+ * @src: string to be copied.
+ *
+ * Return: the pointer to dest.
  */
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
 
 	len = 0;
+
 	while (src[len] != '\0')
 	{
 		len++;
 	}
+
 	for (i = 0; i < len; i++)
 	{
-		dest[i] = '\0';
+		dest[i] = src[i];
 	}
-		return (dest);
+	dest[i] = '\0';
+
+	return (dest);
 }
+
 /**
- * new_dog - creates a new dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
  *
- * Return: pointer to the new dog (Success), NULL otherwise
+ * Return: pointer to the new dog (Success), NULL otherwise.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -60,7 +69,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-	        return (NULL);
+
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
