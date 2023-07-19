6964f73b675b362eb4c3d981e2393afcce8de222
Abdellahsayni <syaniabdos@gmail.com>
2023-07-19 10:52:39 -0700

    ikhan

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,16 +2,16 @@
 #define CALC_H
 
 /**
- * struct op - struct op
+ * struct op - A struct op.
  *
- * #op: The operator
- * #f: The function associated
+ * @op: The operator.
+ * @f: The associated function.
  */
 typedef struct op
 {
 	char *op;
 	int (*f)(int a, int b);
-}op_t;
+} op_t;
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
