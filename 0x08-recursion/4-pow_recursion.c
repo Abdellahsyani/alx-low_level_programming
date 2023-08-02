48f242421188b9a3502a7d7bafea526b9715bf28
Abdellahsyani <syaniabdos@gmail.com>
2023-08-02 16:07:34 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,14 +15,14 @@ int _pow_recursion(int x, int y)
 	}
 	else if (y == 0)
 	{
-		return (0);
+		return (1);
 	}
 	else if (y == 1)
 	{
-		return (1);
+		return (x);
 	}
 	else
 	{
-		return (x = _pow_recursion(x, y - 1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
