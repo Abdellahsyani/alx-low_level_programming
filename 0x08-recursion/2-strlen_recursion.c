6af1d41f675cce8b7e3a0e9006b87d9cfc968f45
Abdellahsyani <syaniabdos@gmail.com>
2023-08-02 02:42:01 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print length the string.
+ * @s: print the length of string.
+ * Return: 0 (succesful)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		_strlen_recursion(s + 1);
+		_putchar(strlen(*s));
+	}
+}
