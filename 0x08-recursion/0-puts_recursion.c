56891878c8712b261ea2ede95a2cebf061ae059c
Abdellahsyani <syaniabdos@gmail.com>
2023-08-02 02:13:37 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string.
+ * @s: print a string follow by new ling.
+ * Return: 0 (succesful)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_puts_recursion(s - 1);
+		_putchar(*s);
+	}
