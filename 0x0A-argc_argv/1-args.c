340a0c03b47278af78e1da882e4b96bb59446524
Abdellahsyani <syaniabdos@gmail.com>
2023-08-04 12:22:54 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print number of argument
+ * @argc: The argument count
+ * @argv: The argument value
+ * Return: 0 (succesfil)
+ */
+
+int main(int argc, char *argv)
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
