cc91f6fae8adb7a334494fba0f9627aa4a1ae151
Abdellahsyani <syaniabdos@gmail.com>
2023-08-08 23:07:41 +0100

    str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,47 +1,46 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * str_concat - print cancocanate
- * @s1: the first string
- * @s2: the seconde string
- * Return: Always 0 successful
+ * _strlen -find length of string
+ * @s: string
+ * Return: int
  */
+int _strlen(char *s)
+{
+	int size = 0;
 
+	for (; s[size] != '\0'; size++)
+	return (size);
+}
+
+/**
+ * str_concat - concatenates two string
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ */
 char *str_concat(char *s1, char *s2)
 {
-char *conct;
-int i, ci;
+	int size1, size2, i;
+	char *m;
 
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
-i = ci = 0;
-while (s1[i] != '\0')
-i++;
-while (s2[ci] != '\0')
-ci++;
-conct = malloc(sizeof(char) * (i + ci + 1));
-if (conct == NULL)
-{
-return (NULL);
-i = ci = 0;
-}
-while (s1[i] != '\0')
-{
-conct[i] = s1[i];
-i++;
-}
-while (s2[ci] != '\0')
-{
-conct[i] = s2[ci];
-i++, ci++;
-}
-conct[i] = '\0';
-return (conct);
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
+	m = malloc((size1 + size2) * sizeof(char) + 1);
+	if (m == 0)
+		return (0);
+
+	for (i = 0; i <= size1 + size2; i++)
+	{
+		if (i < size1)
+			m[i] = s1[i];
+		else
+			m[i] = s2[i - size1];
+	}
+	m[i] = '\0';
+	return (m);
 }
