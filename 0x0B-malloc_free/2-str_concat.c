4d9b86060a33f06b35799107a98ab5904e245f5d
Abdellahsyani <syaniabdos@gmail.com>
2023-08-08 23:15:09 +0100

    str

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,7 @@ int _strlen(char *s)
 	int size = 0;
 
 	for (; s[size] != '\0'; size++)
+	;
 	return (size);
 }
 
@@ -25,14 +26,20 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
+	{
 		s1 = "\0";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "\0";
+	}
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
 	m = malloc((size1 + size2) * sizeof(char) + 1);
 	if (m == 0)
+	{
 		return (0);
+	}
 
 	for (i = 0; i <= size1 + size2; i++)
 	{
