636018ff78b19b7323249f3d4e87aceb8f466606
Abdellahsyani <syaniabdos@gmail.com>
2023-08-11 09:52:37 +0100

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,7 +13,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *ptr;
-	int i;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -26,9 +26,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	unsigned char *bytptr = (unsigned char *)ptr;
+	char *bytptr = (unsigned char *)ptr;
 
-	for (i = 0; i < nmemb * size; i++)
+	for (unsigned int i = 0; i < nmemb * size; i++)
 	{
 		bytptr[i] = 0;
 	}
