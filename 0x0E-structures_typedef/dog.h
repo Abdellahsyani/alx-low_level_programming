bc277cd2bf153744e138ad9cd747677ce6d198f5
Abdellahsyani <syaniabdos@gmail.com>
2023-08-14 22:50:36 +0100

    gp

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,12 +15,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+}
 
 /**
  *  dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -28,4 +27,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /*DOG_H*/
+#endif
