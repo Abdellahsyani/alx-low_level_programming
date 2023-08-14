b4af4c9fb55e77ddeb82456b5eb03b62063c8ec6
Abdellahsyani <syaniabdos@gmail.com>
2023-08-14 11:44:27 +0100

    sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,20 @@
 #ifndef SUM_H
 #define SUM_H
 
-#define SUM(x, y) (SUM = (x) + (y))
+#define SUM(x, y) printf("%d + %d = %d", x, y, x + y)
+#include <stdio.h>
+
+/**
+ * main - sum of function
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	SUM(5, 6);
+
+return (0);
+}
 
 #endif /*SUM_H*/
