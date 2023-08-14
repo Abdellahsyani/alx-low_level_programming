f0206ca262c181f8a19111345126c2560f43d10e
Abdellahsyani <syaniabdos@gmail.com>
2023-08-14 10:48:19 +0100

    all macros

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,6 @@
+#ifndef MACRO_PI_H
+#define MACRO_PI_H
+
+#define PI 3.14159265359
+
+#endif /*MACRO_PI_H*/
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - write name of function
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	char *name;
+
+	if (name == main)
+	{
+		printf("%s\n", name);
+	}
+	else
+	{
+		printf("\n");
+	}
+return (0);
+}
