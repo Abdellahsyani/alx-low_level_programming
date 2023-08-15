933d1d807ba72f0029cd8312579bde1bec15a418
Abdellahsyani <syaniabdos@gmail.com>
2023-08-15 18:57:14 +0100

    4-

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,22 +25,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = malloc(strlen(name) * (len1 + 1));
+	new_dog->name = malloc(sizeof(len1 + 1));
 	if (new_dog->name == NULL)
 	{
-		free(new_dog->name);
-		return (NULL);
-	}
-	new_dog->name = strcpy(new_dog->name, name);
-
-	new_dog->owner = malloc(strlen(owner) * (len2 + 1));
-	if (new_dog->owner == NULL)
-	{
-		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->owner = strcpy(new_dog->owner, owner);
+	strcpy(new_dog->name, name);
+
+	new_dog->owner = malloc(sizeof(len2 + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
 
