a5d15f12c650b9f8fafd1681d65b93c011090195
Abdellahsyani <syaniabdos@gmail.com>
2023-08-24 18:32:19 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)");
 
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%lu] %s\n", h->len, h->str);
 
 		h = h->next;
 		count++;
