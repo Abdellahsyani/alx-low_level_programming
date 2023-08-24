cc11f82c922dc0f69c83451ca76880febd5471f5
Abdellahsyani <syaniabdos@gmail.com>
2023-08-24 13:30:02 +0100

    0-print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print all element of list_t
+ * @h: The string who take the value
+ *
+ * Return: The length of value
+ * writer: abdellahsyani;
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+return (count);
+}
