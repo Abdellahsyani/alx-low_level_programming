974fb5d1d292f6dd6a5da79afc18d1c97b6abe5c
Abdellahsyani <syaniabdos@gmail.com>
2023-08-24 23:04:53 +0100

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - free the list_t list
+ * @head: head of the linked list
+ *
+ */
+
+void free_list(list_t *head)
+{
+	list_t *free_d;
+
+	while ((free_d = head) != NULL)
+	{
+		head = head->next;
+		free(free_d->str);
+		free(free_d);
+	}
+}
