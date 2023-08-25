f7dfb15629e70638903268fc13b80adaa0ffb7ef
Abdellahsyani <syaniabdos@gmail.com>
2023-08-25 11:43:33 +0100

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
 /**
- * fun_print - print a string befor main
+ * main - print a string befor main
  *
  * description: Print a function befor main function
  */
 
-void fun_print(void)
+void __attribute__((constructor)) main()
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-/**
- * main - entry point
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-	fun_print();
-
-return (0);
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
