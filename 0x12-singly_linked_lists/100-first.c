c28043971631f48d23f9fd5ccb8937c1565ec042
Abdellahsyani <syaniabdos@gmail.com>
2023-08-25 11:38:58 +0100

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * fun_print - print a string befor main
+ *
+ * description: Print a function befor main function
+ */
+
+void fun_print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	fun_print();
+
+return (0);
+}
