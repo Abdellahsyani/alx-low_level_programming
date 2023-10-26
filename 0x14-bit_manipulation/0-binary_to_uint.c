d1e7cea96813082390bd4eacf4afc6b8add47994
Abdellahsyani <syaniabdos@gmail.com>
2023-10-26 10:27:13 +0100

    convert

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,24 +9,26 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result;
+	unsigned int binary = 0;
 
-	while (*b != NULL)
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for ( ; *b != '\0'; )
 	{
 		if (*b == '0')
 		{
-			result += result << 1;
+			binary <<= 1;
 		}
 		else if (*b == '1')
 		{
-			result += (result << 1) | 1;
+			binary = (binary << 1) | 1;
 		}
 		else
 		{
 			return (0);
 		}
-		b++;
 	}
-	return (result);
+	return (binary);
 }
-
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdbool.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
