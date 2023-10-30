7a5156fcc7af9bcab0352f851be541bbca06200e
Abdellahsyani <syaniabdos@gmail.com>
2023-10-30 11:58:29 +0100

    create a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <sys/stst.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
