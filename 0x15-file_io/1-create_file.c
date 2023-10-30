1bf3b743026d31377ee0f4beae405e9f8a6cc61c
Abdellahsyani <syaniabdos@gmail.com>
2023-10-30 11:54:31 +0100

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,20 +10,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, len = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[len] != '\0')
+		for (len = 0; text_content[len];)
 			len++;
-		write(fd, text_content, len);
 	}
-	close(fd);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+
 	return (1);
 }
