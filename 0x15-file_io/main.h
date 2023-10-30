e16331ec0ff8d06b6d1d5dc4bd9b78ca26070ed6
Abdellahsyani <syaniabdos@gmail.com>
2023-10-30 12:12:55 +0100

    fix agiian

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,16 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
+/**
+ * File: holberton.h
+ * Auth: kalu 
+ */
 #include <unistd.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <sys/stst.h>
-#include <stdlib.h>
 #include <sys/types.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif
+#endif /*MAIN_H*/
