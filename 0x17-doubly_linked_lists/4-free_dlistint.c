a4710e808e778b3d5ec4c6c48734f7eb00546643
Abdellahsyani <syaniabdos@gmail.com>
2023-12-07 14:07:15 +0100

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,6 +7,16 @@
 
 void free_dlistint(dlistint_t *head)
 {
-	while (head != NULL)
-		free(head);
+	dlistint_t *temp;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+	}
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(temp);
+	}
 }
