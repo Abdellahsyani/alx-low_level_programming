80ff6f02290f3fee0cec020539ef2ff9dee23b3d
Abdellahsyani <syaniabdos@gmail.com>
2024-04-11 17:11:54 +0000

    0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - function that search for the right value
+ * @array: the array that contains the values
+ * @size: the size of the array
+ * @value: the value that we are looking for
+ *
+ * Return: -1 if fails and value if success
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
+
